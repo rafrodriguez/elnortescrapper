176349fea57f97b4eb86f2b95edf8e93b3f3616f
user1 <rafrodriguez@users.noreply.github.com>
2017-03-17 23:06:25 -0600

    listed required packages

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ def readme():
 
 setup(name='elnortescrapper',
     version='1.0.0',
+    install_requires=['lxml','bs4', 'pandas'],
     description='Web scrapper for El Norte Avisos de Ocasión real estate classified ads',
     url='http://github.com/rafrodrz/reformascrapper',
     keywords='elnorte avisos ocasion venta casas anuncios',
